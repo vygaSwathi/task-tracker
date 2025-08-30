9c5b01c6586aafd2670aff3c710da0a46a21d604
vygaSwathi <vygatech09@gmail.com>
2025-08-30 14:43:58 +0530

    modified file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Task from './task';
+import Task from './task'   
 
 function App() {
-  
-
   return (
     <div>
-      <task/>
+      <Task />   
     </div>
   );
 }
 
-export default App;
+export default App
